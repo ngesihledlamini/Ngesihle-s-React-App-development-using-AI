01efaee9cbf04444c790067091af4bd09693a286
Ngesihle <ngesihledlamini05@gmail.com>
2026-08-10 15:01:33 +0200

    Home model implementation

--- src/pages/Home/HomeModel.ts
+++ src/pages/Home/HomeModel.ts
@@ -1,1 +1,13 @@
+import { searchMovies, type Movie } from '../../services/omdbMovieService'
+
+export async function getMovies(query: string): Promise<Movie[]> {
+  const cleanedQuery = query.trim()
+
+  if (cleanedQuery.length < 2) {
+    throw new Error('Search query must contain at least 2 characters.')
+  }
+
+  return searchMovies(cleanedQuery)
+}
+
 export class HomeModel {}
